66ba07b06ff0b7c7804993ccba32b8af8b80b593
Marco <46133335+Marco4413@users.noreply.github.com>
2021-12-23 18:30:47 +0100

    Reworked events/MessageCreate.js

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,44 +1,17 @@
 const { CreateEventListener } = require("../EventListener.js");
-const { SplitCommand, ExecuteCommand } = require("../Command.js");
-const { GetCommands } = require("../Commands.js");
-const { GetLocale } = require("../Localization.js");
-const db = require("../Database.js");
+const Database = require("../Database.js");
 
-const { Client } = require("../Client.js");
-
-/**
- * @param {String} str
- * @returns {Number}
- */
-const _StartsWithBotMention = (str) => {
-    const botMentionMatcher = new RegExp(`^<@!?${Client.user.id}>`, "g");
-    const noMentionStr = str.replace(botMentionMatcher, "");
-    return str.length - noMentionStr.length;
-};
+const _MessageManagers = [
+    require("./MessageCreate/CommandManager.js")
+];
 
 module.exports = CreateEventListener(
     "messageCreate", async msg => {
         if (msg.author.bot || msg.channel.type !== "GUILD_TEXT" || msg.deleted) return;
-        
-        const guild = await db.GetGuild(msg.guild.id);
 
-        const botMentionEnd = _StartsWithBotMention(msg.content);
-        const executeCommand = botMentionEnd > 0 || msg.content.startsWith(guild.prefix);
-
-        if (executeCommand) {
-            const splittedCommand = SplitCommand(
-                msg.content.substring(
-                    botMentionEnd > 0 ? botMentionEnd : guild.prefix.length
-                ).trim()
-            );
-
-            if (splittedCommand.length === 0) return;
-
-            const guildLocale = GetLocale(guild.locale);
-            const commandLocale = { "common": guildLocale.common, "command": { "subcommands": guildLocale.commands } };
-            if (!ExecuteCommand(msg, guild, commandLocale, splittedCommand, GetCommands())) {
-                msg.reply(guildLocale.common.invalidCommand);
-            }
+        const guild = await Database.GetGuild(msg.guild.id);
+        for (let i = 0; i < _MessageManagers.length; i++) {
+            if (await _MessageManagers[i](msg, guild)) return;
         }
     }
 );
---
+++ events/MessageCreate/CommandManager.js
@@ -0,0 +1,46 @@
+const { SplitCommand, ExecuteCommand } = require("../../Command.js");
+const { GetCommands } = require("../../Commands.js");
+const { GetLocale } = require("../../Localization.js");
+const { Client } = require("../../Client.js");
+const DatabaseDefinitions = require("../../DatabaseDefinitions.js");
+
+/**
+ * @param {String} str
+ * @returns {Number}
+ */
+const _StartsWithBotMention = (str) => {
+    const botMentionMatcher = new RegExp(`^<@!?${Client.user.id}>`, "g");
+    const noMentionStr = str.replace(botMentionMatcher, "");
+    return str.length - noMentionStr.length;
+};
+
+/**
+ * This module Manages Commands
+ * @param {Message} msg
+ * @param {DatabaseDefinitions.GuildRow} guild
+ * @returns {Boolean}
+ */
+module.exports = async (msg, guild) => {
+    const botMentionEnd = _StartsWithBotMention(msg.content);
+    const executeCommand = botMentionEnd > 0 || msg.content.startsWith(guild.prefix);
+
+    if (executeCommand) {
+        const splittedCommand = SplitCommand(
+            msg.content.substring(
+                botMentionEnd > 0 ? botMentionEnd : guild.prefix.length
+            ).trim()
+        );
+
+        if (splittedCommand.length === 0) return true;
+
+        const guildLocale = GetLocale(guild.locale);
+        const commandLocale = { "common": guildLocale.common, "command": { "subcommands": guildLocale.commands } };
+        if (!ExecuteCommand(msg, guild, commandLocale, splittedCommand, GetCommands())) {
+            await msg.reply(guildLocale.common.invalidCommand);
+        }
+
+        return true;
+    }
+
+    return false;
+};
