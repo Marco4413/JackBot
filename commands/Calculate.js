bdddd9525990a0cda2dc63bacb0f35c1b9c04007
Marco <46133335+Marco4413@users.noreply.github.com>
2021-12-26 14:54:30 +0100

    Added Calculate Command

---
+++ commands/Calculate.js
@@ -0,0 +1,19 @@
+const { CreateCommand, Utils } = require("../Command.js");
+const SMath = require("../SandboxedMath.js");
+
+module.exports = CreateCommand({
+    "name": "calculate",
+    "shortcut": "calc",
+    "execute": async (msg, guild, locale, args) => {
+        const mathExpr = Utils.JoinArray(args, " ");
+        try {
+            const result = SMath.evaluate(mathExpr);
+            await msg.reply(Utils.FormatString(
+                locale.command.expressionResult,
+                mathExpr, Utils.TranslateNumber(result, locale)
+            ));
+        } catch (err) {
+            await msg.reply(locale.command.expressionError);
+        }
+    }
+});
