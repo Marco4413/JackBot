16844c3b99432e4b01192664a0db15e72eea6894
Marco <46133335+Marco4413@users.noreply.github.com>
2021-12-29 00:55:01 +0100

    Requiring Logger at the top of index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const Logger = require("./Logger.js");
 const { StartRichPresence } = require("./RichPresence.js");
 const { Client, RegisterEventListeners } = require("./Client.js");
 const { RegisterCommands } = require("./Commands.js");
 const { RegisterLocales } = require("./Localization.js");
 const db = require("./Database.js");
-const Logger = require("./Logger.js");
 
 (async () => {
     Logger.TimeStart("StartupTime");
