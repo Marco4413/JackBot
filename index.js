4232ab571f1382f304c00ce43cc9c439989d5f9a
Marco <46133335+Marco4413@users.noreply.github.com>
2021-12-29 01:23:06 +0100

    Added Error Logging for the main function

--- index.js
+++ index.js
@@ -53,4 +53,16 @@ const db = require("./Database.js");
     Logger.GroupEnd();
     
     Logger.TimeEnd("StartupTime");
-})();
+})().catch(async error => {
+    // Logging any error that happens within the main function
+    Logger.Error(error.stack);
+
+    const _INTERRUPT_STAGES_DELAY = 1000; // 1 Second should be more than enough
+    // This delay is to make sure that the Logger has Logged the Error
+    await new Promise(r => setTimeout(r, _INTERRUPT_STAGES_DELAY));
+    // Adding some delay if the event has listeners to make sure that async functions have terminated
+    if (process.emit("SIGINT")) await new Promise(r => setTimeout(r, _INTERRUPT_STAGES_DELAY));
+
+    // "Gracefully" exiting
+    process.exit(0);
+});
