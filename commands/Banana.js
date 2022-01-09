50d8bac475d34747d42def1a1204cfd40db5a39e
Marco <46133335+Marco4413@users.noreply.github.com>
2022-01-09 23:58:06 +0100

    Added Banana Command

---
+++ commands/Banana.js
@@ -0,0 +1,20 @@
+const { CreateCommand, Utils } = require("../Command.js");
+const { TenorSearch } = require("../Tenor.js");
+
+module.exports = CreateCommand({
+    "name": "banana",
+    "shortcut": "b",
+    "execute": async (msg, guild, locale) => {
+        const gif = Utils.GetRandomArrayElement(
+            await TenorSearch("banana", 50, "search", "high", "minimal")
+        );
+
+        if (gif === undefined) {
+            await msg.reply("No 🍌 was Found :(");
+        } else {
+            const embed = Utils.GetDefaultEmbedForMessage(msg, false)
+                .setImage(gif.media[0].gif.url);
+            await msg.channel.send({ "embeds": [ embed ] });
+        }
+    }
+});
