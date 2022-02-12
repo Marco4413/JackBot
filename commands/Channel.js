0a89fa77d3cbc360ea4c34f0e12e861336d3b6c5
Marco <46133335+Marco4413@users.noreply.github.com>
2022-02-12 23:05:41 +0100

    Reworked Private Channels
    This was really needed, the previous code was a mess.

--- commands/Channel.js
+++ commands/Channel.js
@@ -1,134 +1,10 @@
-const { GuildMember, GuildChannel } = require("discord.js");
 const Sequelize = require("sequelize");
 const { Client } = require("../Client.js");
 const { CreateCommand, Permissions, Database, Utils } = require("../Command.js");
-const { Locale } = require("../Localization.js");
 const Logger = require("../Logger.js");
 const { CreateInterval } = require("../Timing.js");
 const { ReplyIfBlacklisted } = require("./utils/AccessListUtils.js");
-
-/**
- * @param {"GUILD_TEXT"|"GUILD_VOICE"} channelType
- * @param {keyof import("../DatabaseDefinitions.js").UserRow} channelIdRow
- * @param {String} [defaultChannelName]
- * @param {(member: GuildMember, channel: GuildChannel, locale: Locale) => Promise<Void>} [onCreate]
- * @param {(member: GuildMember, channel: GuildChannel, locale: Locale) => Promise<Void>} [onDelete]
- * @returns {import("../Command.js").Command[]}
- */
-const _GetChannelSubcommands = (channelType, channelIdRow, defaultChannelName = "{0}'s Channel", onCreate, onDelete) => {
-    return [
-        {
-            "name": "create",
-            "shortcut": "c",
-            "execute": async (msg, guild, locale, args) => {
-                const user = await Database.GetOrCreateRow("user", {
-                    "guildId": msg.guildId,
-                    "userId": msg.member.id
-                });
-
-                if (user[channelIdRow] !== null) {
-                    const channel = msg.guild.channels.resolve(user[channelIdRow]);
-                    if (channel !== null) {
-                        if (!channel.deletable) {
-                            await msg.reply(locale.Get("cantDelete"));
-                            return;
-                        }
-    
-                        if (onDelete !== undefined)
-                            await onDelete(msg.member, channel, locale);
-                        await channel.delete(`${msg.member.id} asked for a new ${channelType} Channel.`);
-                    }
-                }
-                
-                /** @type {Permissions} */
-                let everyonePermissions = msg.guild.roles.everyone.permissions;
-                if (guild.privateChannelEveryoneTemplateRoleId !== null) {
-                    const everyoneTemplateRole = msg.guild.roles.resolve(
-                        guild.privateChannelEveryoneTemplateRoleId
-                    );
-                    
-                    if (everyoneTemplateRole !== null)
-                        everyonePermissions = everyoneTemplateRole.permissions;
-                }
-
-                /** @type {Permissions} */
-                let ownerPermissions = msg.guild.roles.everyone.permissions;
-                if (guild.privateChannelOwnerTemplateRoleId !== null) {
-                    const ownerTemplateRole = msg.guild.roles.resolve(
-                        guild.privateChannelOwnerTemplateRoleId
-                    );
-                    
-                    if (ownerTemplateRole !== null)
-                        ownerPermissions = ownerTemplateRole.permissions;
-                }
-
-                const channelName = args.length === 0 ?
-                    Utils.FormatString(defaultChannelName, msg.member.displayName) :
-                    Utils.JoinArray(args, " ");
-                
-                const parent = msg.guild.channels.resolve(guild.privateChannelCategoryId);
-                const channel = await msg.guild.channels.create(channelName, {
-                    "type": channelType,
-                    "parent": parent?.type === "GUILD_CATEGORY" ? parent : null,
-                    "permissionOverwrites": [
-                        {
-                            "id": msg.guild.roles.everyone.id,
-                            "type": "role",
-                            "allow": everyonePermissions
-                        },
-                        {
-                            "id": msg.member.id,
-                            "type": "member",
-                            "allow": ownerPermissions
-                        }
-                    ]
-                });
-
-                await Database.SetRowAttr("user", {
-                    "guildId": msg.guildId,
-                    "userId": msg.member.id
-                }, { [channelIdRow]: channel.id });
-
-                if (!msg.deleted) await msg.reply(locale.Get("created"));
-                if (onCreate !== undefined)
-                    await onCreate(msg.member, channel, locale);
-            }
-        },
-        {
-            "name": "delete",
-            "shortcut": "d",
-            "execute": async (msg, guild, locale) => {
-                const user = await Database.GetOrCreateRow("user", {
-                    "guildId": msg.guildId,
-                    "userId": msg.member.id
-                });
-
-                if (user[channelIdRow] === null) {
-                    await msg.reply(locale.Get("noChannel"));
-                    return;
-                }
-
-                const channel = msg.guild.channels.resolve(user[channelIdRow]);
-                if (channel === null) {
-                    await msg.reply(locale.Get("noChannel"));
-                } else if (!channel.deletable) {
-                    await msg.reply(locale.Get("cantDelete"));
-                    return;
-                } else {
-                    await msg.reply(locale.Get("deleted"));
-                    if (onDelete !== undefined)
-                        await onDelete(msg.member, channel, locale);
-                    await channel.delete(`${msg.member.id} asked to delete his ${channelType} Channel.`);
-                }
-
-                await Database.SetRowAttr("user", {
-                    "guildId": msg.guildId,
-                    "userId": msg.member.id
-                }, { [channelIdRow]: null });
-            }
-        }
-    ];
-};
+const { CreateTextChannel, DeleteTextChannel, CreateVoiceChannel, DeleteVoiceChannel } = require("./utils/ChannelUtils.js");
 
 const _CHANNEL_SWEEP_INTERVAL = Utils.GetEnvVariable(
     "CHANNEL_SWEEP_INTERVAL", Utils.AnyToNumber, 900e3, Logger.Warn
@@ -168,7 +44,9 @@ CreateInterval(async id => {
 
         if (user.privateTextChannelId !== null) {
             const textChannel = guild.channels.resolve(user.privateTextChannelId);
-            if (textChannel !== null && (
+            if (textChannel === null || textChannel.deleted) {
+                textChannelDeleted = true;
+            } else if ((
                 // If the last message was sent before the max inactivity time
                 //  or if none the channel wasn't just created then ...
                 textChannel.lastMessage === null ?
@@ -184,7 +62,9 @@ CreateInterval(async id => {
 
         if (user.privateVoiceChannelId !== null) {
             const voiceChannel = guild.channels.resolve(user.privateVoiceChannelId);
-            if (voiceChannel !== null &&
+            if (voiceChannel === null || voiceChannel.deleted) {
+                voiceChannelDeleted = true;
+            } else if (
                 voiceChannel.members.size === 0 &&
                 (dateNow - voiceChannel.createdTimestamp) >= _CHANNEL_JUST_CREATED_THRESHOLD &&
                 voiceChannel.deletable
@@ -218,44 +98,42 @@ module.exports = CreateCommand({
         {
             "name": "text",
             "shortcut": "t",
-            "subcommands": _GetChannelSubcommands(
-                "GUILD_TEXT", "privateTextChannelId", "{0}-text-channel",
-                async (member, channel, locale) => {
-                    await channel.send(locale.GetFormatted(
-                        "welcome", Utils.MentionUser(member.id)
-                    ));
+            "subcommands": [
+                {
+                    "name": "create",
+                    "shortcut": "c",
+                    "execute": async (msg, guild, locale, args) => {
+                        await CreateTextChannel(msg.member, args.length > 0 ? Utils.JoinArray(args, " ") : undefined, msg);
+                    }
+                },
+                {
+                    "name": "delete",
+                    "shortcut": "d",
+                    "execute": async (msg, guild, locale) => {
+                        await DeleteTextChannel(msg.member, msg);
+                    }
                 }
-            )
+            ]
         },
         {
             "name": "voice",
             "shortcut": "v",
-            "subcommands": _GetChannelSubcommands(
-                "GUILD_VOICE", "privateVoiceChannelId", "{0}'s Voice Channel",
-                async (member, channel) => {
-                    if (member.voice.channelId !== null) {
-                        await member.voice.setChannel(channel, `${member.id} created a new private GUILD_VOICE Channel.`);
+            "subcommands": [
+                {
+                    "name": "create",
+                    "shortcut": "c",
+                    "execute": async (msg, guild, locale, args) => {
+                        await CreateVoiceChannel(msg.member, args.length > 0 ? Utils.JoinArray(args, " ") : undefined, msg);
                     }
                 },
-                async (member, channel) => {
-                    // Getting All Voice Channels of the Guild except the one that is being deleted
-                    const channels = (await member.guild.channels.fetch())
-                        .filter(ch => ch.isVoice() && ch.id !== channel.id);
-
-                    // For each channel member
-                    for (const member of channel.members.values()) {
-                        // If it's still connected
-                        if (member.voice.channelId !== null) {
-                            // Get a random channel that it can join
-                            const channel = channels.filter(
-                                ch => ch.permissionsFor(member).has(Permissions.FLAGS.CONNECT)
-                            ).random();
-                            // Set its channel to the one picked or disconnect if none
-                            await member.voice.setChannel(channel ?? null, "GUILD_VOICE Channel Deleted.");
-                        }
+                {
+                    "name": "delete",
+                    "shortcut": "d",
+                    "execute": async (msg, guild, locale) => {
+                        await DeleteVoiceChannel(msg.member, true, msg);
                     }
                 }
-            )
+            ]
         },
         {
             "name": "category",
@@ -384,7 +262,7 @@ module.exports = CreateCommand({
                             if (templateRole === null) {
                                 await msg.reply(locale.Get("invalidRole"));
                                 return;
-                            } else if (templateRole.comparePositionTo(msg.member.roles.highest) <= 0) {
+                            } else if (templateRole.comparePositionTo(msg.member.roles.highest) >= 0) {
                                 await msg.reply(locale.Get("higherRole"));
                                 return;
                             } else if (!msg.member.permissions.has(templateRole.permissions, true)) {
@@ -438,7 +316,7 @@ module.exports = CreateCommand({
                             if (templateRole === null) {
                                 await msg.reply(locale.Get("invalidRole"));
                                 return;
-                            } else if (templateRole.comparePositionTo(msg.member.roles.highest) <= 0) {
+                            } else if (templateRole.comparePositionTo(msg.member.roles.highest) >= 0) {
                                 await msg.reply(locale.Get("higherRole"));
                                 return;
                             } else if (!msg.member.permissions.has(templateRole.permissions, true)) {
---
+++ commands/utils/ChannelUtils.js
@@ -0,0 +1,264 @@
+const { GuildMember, Message, Permissions } = require("discord.js");
+const Database = require("../../Database.js");
+const { GetLocale } = require("../../Localization");
+const Utils = require("../../Utils.js");
+
+/**
+ * Creates a new Voice Channel for the specified Member
+ * @param {GuildMember} member The Member to create the new Voice Channel for
+ * @param {String} [channelName] The name of the new Channel
+ * @param {Message?} [msg] The Message to reply to for verbose ( None if null or undefined )
+ */
+const CreateVoiceChannel = async (member, channelName, msg) => {
+    const guild = await Database.GetOrCreateRow("guild", { "id": member.guild.id });
+
+    let locale;
+    if ((msg ?? undefined) !== undefined) {
+        locale = GetLocale(guild.locale).GetCommandLocale([ "channel", "voice" ]);
+    }
+
+    const user = await Database.GetOrCreateRow("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    });
+
+    let userChannel = member.guild.channels.resolve(user.privateVoiceChannelId);
+    if (userChannel === null || userChannel.deleted) {
+        const parent = member.guild.channels.resolve(guild.privateChannelCategoryId);
+        const permissionOverwrites = [ ];
+        if (guild.privateChannelEveryoneTemplateRoleId !== null) {
+            const everyoneTemplateRole = member.guild.roles.resolve(
+                guild.privateChannelEveryoneTemplateRoleId
+            );
+            
+            if (everyoneTemplateRole !== null) {
+                permissionOverwrites.push({
+                    "id": member.guild.roles.everyone.id,
+                    "type": "role",
+                    "allow": parent?.permissionsFor(everyoneTemplateRole) ?? everyoneTemplateRole.permissions
+                });
+            }
+        }
+    
+        if (guild.privateChannelOwnerTemplateRoleId !== null) {
+            const ownerTemplateRole = member.guild.roles.resolve(
+                guild.privateChannelOwnerTemplateRoleId
+            );
+    
+            if (ownerTemplateRole !== null) {
+                permissionOverwrites.push({
+                    "id": member.id,
+                    "type": "member",
+                    "allow": parent?.permissionsFor(ownerTemplateRole) ?? ownerTemplateRole.permissions
+                });
+            }
+        }
+
+        userChannel = await member.guild.channels.create(
+            channelName ?? `${member.displayName}'s Voice Channel`, {
+                "type": "GUILD_VOICE",
+                "parent": parent?.type === "GUILD_CATEGORY" ? parent : null,
+                permissionOverwrites
+            }
+        );
+
+        await Database.SetRowAttr("user", {
+            "guildId": member.guild.id,
+            "userId": member.id
+        }, { "privateVoiceChannelId": userChannel.id });
+
+        await msg?.reply(locale.Get("created"));
+    } else {
+        await msg?.reply(locale.Get("alreadyCreated"));
+    }
+
+    if (member.voice.channelId !== null)
+        await member.voice.setChannel(userChannel, `${member.id} created a new Private Voice Channel.`);
+};
+
+/**
+ * Deletes the Private Voice Channel for the specified Member
+ * @param {GuildMember} member The Member to delete the Voice Channel for
+ * @param {Boolean} [scatterUsers] Whether or not users in the Channel should be scattered around the Guild
+ * @param {Message?} [msg] The Message to reply to for verbose ( None if null or undefined )
+ */
+const DeleteVoiceChannel = async (member, scatterUsers = true, msg) => {
+    let locale;
+    if ((msg ?? undefined) !== undefined) {
+        const guild = await Database.GetOrCreateRow("guild", { "id": member.guild.id });
+        locale = GetLocale(guild.locale).GetCommandLocale([ "channel", "voice" ]);
+    }
+
+    const user = await Database.GetOrCreateRow("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    });
+
+    if (user.privateVoiceChannelId === null) {
+        await msg?.reply(locale.Get("noChannel"));
+        return;
+    }
+
+    const userChannel = member.guild.channels.resolve(user.privateVoiceChannelId);
+    if (userChannel === null || userChannel.deleted) {
+        await msg?.reply(locale.Get("noChannel"));
+        return;
+    }
+
+    if (!userChannel.deletable) {
+        await msg?.reply(locale.Get("cantDelete"));
+        return;
+    }
+    
+    if (scatterUsers) {
+        const guild = await Database.GetOrCreateRow("guild", { "id": member.guild.id });
+
+        // Getting All Voice Channels of the Guild except the one that is being deleted
+        const channels = (await member.guild.channels.fetch()).filter(ch =>
+            ch.isVoice() && ch.id !== userChannel.id &&
+            ch.id !== guild.privateVoiceCreateChannelId &&
+            ch.permissionsFor(member.guild.me)
+                .has(Permissions.FLAGS.MOVE_MEMBERS)
+        );
+
+        // For each channel member
+        for (const chMember of userChannel.members.values()) {
+            // If it's still connected
+            if (chMember.voice.channelId !== null) {
+                // Get a random channel that it can join
+                const channel = channels.filter(
+                    ch => ch.permissionsFor(chMember).has(Permissions.FLAGS.CONNECT | Permissions.FLAGS.VIEW_CHANNEL)
+                ).random();
+                // Set its channel to the one picked or disconnect if none
+                await chMember.voice.setChannel(channel ?? null, `${chMember.id}'s Voice Channel was Deleted.`);
+            }
+        }
+    }
+
+    await userChannel.delete(`${member.id} asked to delete its Private Voice Channel.`);
+    await Database.SetRowAttr("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    }, { "privateVoiceChannelId": null });
+    await msg?.reply(locale.Get("deleted"));
+};
+
+/**
+ * Creates a new Text Channel for the specified Member
+ * @param {GuildMember} member The Member to create the new Text Channel for
+ * @param {String} [channelName] The name of the new Channel
+ * @param {Message?} [msg] The Message to reply to for verbose ( None if null or undefined )
+ */
+const CreateTextChannel = async (member, channelName, msg) => {
+    const guild = await Database.GetOrCreateRow("guild", { "id": member.guild.id });
+
+    let locale;
+    if ((msg ?? undefined) !== undefined) {
+        locale = GetLocale(guild.locale).GetCommandLocale([ "channel", "text" ]);
+    }
+
+    const user = await Database.GetOrCreateRow("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    });
+
+    let userChannel = member.guild.channels.resolve(user.privateTextChannelId);
+    if (userChannel === null || userChannel.deleted) {
+        const parent = member.guild.channels.resolve(guild.privateChannelCategoryId);
+        const permissionOverwrites = [ ];
+        if (guild.privateChannelEveryoneTemplateRoleId !== null) {
+            const everyoneTemplateRole = member.guild.roles.resolve(
+                guild.privateChannelEveryoneTemplateRoleId
+            );
+            
+            if (everyoneTemplateRole !== null) {
+                permissionOverwrites.push({
+                    "id": member.guild.roles.everyone.id,
+                    "type": "role",
+                    "allow": parent?.permissionsFor(everyoneTemplateRole) ?? everyoneTemplateRole.permissions
+                });
+            }
+        }
+    
+        if (guild.privateChannelOwnerTemplateRoleId !== null) {
+            const ownerTemplateRole = member.guild.roles.resolve(
+                guild.privateChannelOwnerTemplateRoleId
+            );
+    
+            if (ownerTemplateRole !== null) {
+                permissionOverwrites.push({
+                    "id": member.id,
+                    "type": "member",
+                    "allow": parent?.permissionsFor(ownerTemplateRole) ?? ownerTemplateRole.permissions
+                });
+            }
+        }
+
+        userChannel = await member.guild.channels.create(
+            channelName ?? `${member.displayName}'s Text Channel`, {
+                "type": "GUILD_TEXT",
+                "parent": parent?.type === "GUILD_CATEGORY" ? parent : null,
+                permissionOverwrites
+            }
+        );
+
+        await Database.SetRowAttr("user", {
+            "guildId": member.guild.id,
+            "userId": member.id
+        }, { "privateTextChannelId": userChannel.id });
+
+        await msg?.reply(locale.Get("created"));
+        
+        await userChannel.send(locale.GetFormatted(
+            "welcome", Utils.MentionUser(member.id)
+        ));
+    } else {
+        await msg?.reply(locale.Get("alreadyCreated"));
+    }
+};
+
+/**
+ * Deletes the Private Text Channel for the specified Member
+ * @param {GuildMember} member The Member to delete the Text Channel for
+ * @param {Message?} [msg] The Message to reply to for verbose ( None if null or undefined )
+ */
+const DeleteTextChannel = async (member, msg) => {
+    let locale;
+    if ((msg ?? undefined) !== undefined) {
+        const guild = await Database.GetOrCreateRow("guild", { "id": member.guild.id });
+        locale = GetLocale(guild.locale).GetCommandLocale([ "channel", "text" ]);
+    }
+
+    const user = await Database.GetOrCreateRow("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    });
+
+    if (user.privateTextChannelId === null) {
+        await msg?.reply(locale.Get("noChannel"));
+        return;
+    }
+
+    const channel = member.guild.channels.resolve(user.privateTextChannelId);
+    if (channel === null || channel.deleted) {
+        await msg?.reply(locale.Get("noChannel"));
+        return;
+    }
+
+    if (!channel.deletable) {
+        await msg?.reply(locale.Get("cantDelete"));
+        return;
+    }
+
+    await channel.delete(`${member.id} asked to delete its Private Text Channel.`);
+    await Database.SetRowAttr("user", {
+        "guildId": member.guild.id,
+        "userId": member.id
+    }, { "privateTextChannelId": null });
+    if (!((msg ?? undefined) === undefined || msg.deleted)) await msg?.reply(locale.Get("deleted"));
+};
+
+module.exports = {
+    CreateVoiceChannel, DeleteVoiceChannel,
+    CreateTextChannel, DeleteTextChannel
+};
