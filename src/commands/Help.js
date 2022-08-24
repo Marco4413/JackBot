5a49301dbd87e25a0bbd341e161bd38c715cc171
Marco <46133335+Marco4413@users.noreply.github.com>
2022-08-24 16:25:43 +0200

    Improved Help Docs formatting

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -1,13 +1,18 @@
 const { CreateCommand, Utils } = require("../Command.js");
 
+const _NBSP = String.fromCodePoint(12644);
+
 /**
  * @param {{ title: String, hidden: Boolean|undefined, hiddenNames: String[]|undefined }} commandDoc
  * @param {String} [titleOverride]
+ * @param {Boolean} [forceName]
+ * @param {Boolean} [ignoreHidden]
+ * @param {Record<String, Any>} [titleFormats]
  * @returns {String}
  */
-const _GetCommandDocName = (commandDoc, titleOverride, forceName = true) => {
+const _GetCommandDocName = (commandDoc, titleOverride, forceName = true, ignoreHidden = false, titleFormats = { }) => {
     const isHidden = commandDoc.hidden;
-    if (!isHidden) return titleOverride ?? commandDoc.title;
+    if (ignoreHidden || !isHidden) return titleOverride ?? Utils.MapFormatString(commandDoc.title, titleFormats);
 
     if (Array.isArray(commandDoc.hiddenNames)) {
         if (commandDoc.hiddenNames.length > 0) {
@@ -43,45 +48,63 @@ module.exports = CreateCommand({
             localePath.push("subcommands", docsPath[i]);
 
         const commandDoc = locale.GetSubLocale(localePath) ?? locale.GetSubLocale("emptyDoc");
-        const commandTitle = commandDoc.Get("title", false);
-
         const noTitle = locale.Get("noTitle");
-        const embed = Utils.GetDefaultEmbedForMessage(msg, true)
-            .setTitle(commandTitle === null ? docsPath[docsPath.length - 1] : (commandTitle ?? noTitle))
-            .setDescription(
-                _HandleDescription(commandDoc.Get("longDescription", false), null) ??
-                _HandleDescription(commandDoc.Get("description", false), "")
-            );
-
+        const embed = Utils.GetDefaultEmbedForMessage(
+            msg, true
+        ).setTitle(
+            commandDoc.Get("fullTitle", false) ??
+            _GetCommandDocName(commandDoc._locale, null, undefined, true, { "this-key": docsPath[docsPath.length - 1] }) ??
+            noTitle
+        ).setDescription(
+            _HandleDescription(commandDoc.Get("longDescription", false), null) ??
+            _HandleDescription(commandDoc.Get("description", false), "")
+        );
+        
+        {
+            const thumbnailUrl = commandDoc.Get("thumbnailUrl", false);
+            if (thumbnailUrl != null) embed.setThumbnail(thumbnailUrl);
+            const authorName = commandDoc.Get("authorName", false);
+            if (authorName != null) embed.author.name = authorName;
+            const authorIconUrl = commandDoc.Get("authorIconUrl", false);
+            if (authorIconUrl != null) embed.author.iconURL = authorIconUrl;
+            const imageUrl = commandDoc.Get("imageUrl", false);
+            if (imageUrl != null) embed.setImage(imageUrl);
+        }
+        
         const subcommands = commandDoc.GetSubLocale("subcommands");
         if (subcommands != null) {
             for (const subKey of Object.keys(subcommands._locale)) {
                 const subDoc = subcommands.Get(subKey, false);
                 if (subDoc.hidden) {
                     const name = _GetCommandDocName(subDoc, noTitle, false);
-                    if (name != null) embed.addField(name, locale.Get("noSubcommands"), false);
+                    if (name != null) embed.addField(name, subDoc.hideSubcommandsPreview === true ? _NBSP : locale.Get("noSubcommands"), false);
                     continue;
                 }
 
-                let subSubCmdList;
-                if (subDoc.subcommands == null) {
-                    subSubCmdList = locale.Get("noSubcommands");
-                } else {
-                    const subSubcommands = subcommands.GetSubLocale([ subKey, "subcommands" ]);
-                    subSubCmdList = locale.GetFormatted(
-                        "subcommandsList", {
-                            "commands": locale.GetFormattedInlineList(
-                                Object.keys(subSubcommands._locale),
-                                null,
-                                el => ({ "value": _GetCommandDocName(subSubcommands.Get(el, false), el) })
-                            )
-                        }
-                    );
+                let subSubCmdList = null;
+                if (subDoc.hideSubcommandsPreview !== true) {
+                    if (subDoc.subcommands == null) {
+                        subSubCmdList = locale.Get("noSubcommands");
+                    } else {
+                        const subSubcommands = subcommands.GetSubLocale([ subKey, "subcommands" ]);
+                        subSubCmdList = locale.GetFormatted(
+                            "subcommandsList", {
+                                "commands": locale.GetFormattedInlineList(
+                                    Object.keys(subSubcommands._locale),
+                                    null,
+                                    el => ({ "value": _GetCommandDocName(subSubcommands.Get(el, false), el) })
+                                )
+                            }
+                        );
+                    }
                 }
 
-                embed.addField(subDoc.title === null ? subKey : (subDoc.title ?? noTitle), `${
+                const fieldDescription = `${
                     _HandleDescription(subDoc.description, "")
-                }\n${subSubCmdList}`, false);
+                }\n${subSubCmdList ?? ""}`.trim();
+                
+                embed.addField(_GetCommandDocName(subDoc, null, undefined, true, { "this-key": subKey }),
+                    fieldDescription.length > 0 ? fieldDescription : _NBSP, false);
             }
         }
 
