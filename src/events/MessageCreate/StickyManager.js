34a3cafffbf69b631bc3054859825a5289c3f904
Marco <46133335+Marco4413@users.noreply.github.com>
2022-10-02 13:23:12 +0200

    Hopefully fixed issues with Sticky Messages

--- src/events/MessageCreate/StickyManager.js
+++ src/events/MessageCreate/StickyManager.js
@@ -15,37 +15,25 @@ module.exports = async (msg) => {
     });
 
     if (channelRow?.stickyMessageId != null) {
-        await Utils.LockTask("StickyManager");
-        try {
-            if (msg.id === channelRow.stickyMessageId) return true;
+        if (msg.id === channelRow.stickyMessageId) return true;
+
+        const timeSinceLastChange = (
+            Date.now() - channelRow.updatedAt.getTime()
+        );
+
+        if (timeSinceLastChange >= _STICKY_MESSAGE_COOLDOWN) {
+            const stickyMessage = await Utils.SafeFetch(msg.channel.messages, channelRow.stickyMessageId);
+            if (stickyMessage != null) {
+                await Utils.SafeDelete(stickyMessage);
+                const newStickyMessage = await (stickyMessage.content.length > 0 ?
+                    msg.channel.send(stickyMessage.content) :
+                    msg.channel.send({ "embeds": [ stickyMessage.embeds[0] ] })
+                );
     
-            const timeSinceLastChange = (
-                Date.now() - channelRow.updatedAt.getTime()
-            );
-    
-            if (timeSinceLastChange >= _STICKY_MESSAGE_COOLDOWN) {
-                const stickyMessage = await Utils.SafeFetch(msg.channel.messages, channelRow.stickyMessageId);
-                if (stickyMessage != null) {
-                    await Utils.SafeDelete(stickyMessage);
-                    const newStickyMessage = await (stickyMessage.content.length > 0 ?
-                        msg.channel.send(stickyMessage.content) :
-                        msg.channel.send({ "embeds": [ stickyMessage.embeds[0] ] })
-                    );
-        
-                    await Database.SetRowAttr("channel", {
-                        "guildId": msg.guildId, "channelId": msg.channelId
-                    }, { "stickyMessageId": newStickyMessage.id });
-                } else {
-                    Logger.Warn(`Reference to Sticky Message in ${Utils.MentionChannel(channelRow.channelId)}@${channelRow.guildId} was lost.`);
-                    await Database.SetRowAttr("channel", {
-                        "guildId": msg.guildId, "channelId": msg.channelId
-                    }, { "stickyMessageId": null });
-                }
+                await Database.SetRowAttr("channel", {
+                    "guildId": msg.guildId, "channelId": msg.channelId
+                }, { "stickyMessageId": newStickyMessage.id });
             }
-        } catch (error) {
-            Logger.Error(error);
-        } finally {
-            Utils.UnlockTask("StickyManager");
         }
     }
 
